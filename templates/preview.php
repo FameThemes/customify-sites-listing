6f679190d35f2d29c65f119598f7c6ae4529b110
TruongSa <shrimp2t@gmail.com>
2018-08-20 16:09:27 +0700

    Update template file

--- templates/preview.php
+++ templates/preview.php
@@ -4,7 +4,9 @@
             <div class="cs-preview-loading">
                 <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#333"> <g fill="none" fill-rule="evenodd"> <g transform="translate(1 1)" stroke-width="2"> <circle stroke-opacity=".5" cx="18" cy="18" r="18"/> <path d="M36 18c0-9.94-8.06-18-18-18"> <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/> </path> </g> </g></svg>
             </div>
-            <iframe id="cs-preview-iframe" src=""></iframe>
+            <div class="cs-iframe-wrapper">
+                <iframe id="cs-preview-iframe" frameborder="0" hspace="0" allowtransparency="true" src=""></iframe>
+            </div>
 
         </div>
     </div>
